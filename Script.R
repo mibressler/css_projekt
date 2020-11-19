6b7e27e71ffe65137a76226060d25c74041e0a02
Michael Bressler <53796824+mbressler@users.noreply.github.com>
2020-11-19 13:26:57 +0100

    data

--- Script.R
+++ Script.R
@@ -1,1 +1,9 @@
+library(tidyverse)
+library(readr)
 
+coronaNet <- read.csv('data/coronanet_release.csv')
+germany <- coronaNet %>% filter (country == "Germany")
+x <- table(germany$type)
+
+plot(x)
+               
