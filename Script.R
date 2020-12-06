ef552f57464748bfb928441b2db328e39e192b95
isarusso <75578427+isarusso@users.noreply.github.com>
2020-12-06 17:21:57 +0000

    update

--- Script.R
+++ Script.R
@@ -1,9 +1,9 @@
+setwd("/Users/isa/Documents/POLITIKWISSENSCHAFT/5. SEMESTER/Computational Social Science/css_projekt")
+
 library(tidyverse)
 library(readr)
-
 coronaNet <- read.csv('data/coronanet_release.csv')
 germany <- coronaNet %>% filter (country == "Germany")
 x <- table(germany$type)
 
 plot(x)
-               
