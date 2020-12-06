47dd8c918cb1ee4f72bfe72a6f8b7915472b581d
Michael Bressler <53796824+michael-eu@users.noreply.github.com>
2020-12-06 18:27:50 +0100

    Update Script.R

--- Script.R
+++ Script.R
@@ -1,4 +1,4 @@
-setwd("/Users/isa/Documents/POLITIKWISSENSCHAFT/5. SEMESTER/Computational Social Science/css_projekt")
+
 
 library(tidyverse)
 library(readr)
@@ -7,8 +7,3 @@ germany <- coronaNet %>% filter (country == "Germany")
 x <- table(germany$type)
 
 plot(x)
-<<<<<<< HEAD
-=======
-               
-y=5
->>>>>>> f1d95ab4dab6a55f5dcd291d327b1801374460d6
