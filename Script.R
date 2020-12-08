8219302346afe0d02da5d03a2e234253f68777d5
Michael Bressler <53796824+mbressler@users.noreply.github.com>
2020-12-08 17:44:22 +0100

    added casedata

--- Script.R
+++ Script.R
@@ -3,6 +3,9 @@
 library(tidyverse)
 library(readr)
 coronaNet <- read.csv('data/coronanet_release.csv')
+casedata <- read.csv('data/owid-covid-data.csv')
+
+germanycase <- casedata %>% filter (location == "Germany")
 germany <- coronaNet %>% filter (country == "Germany")
 x <- table(germany$type)
 
