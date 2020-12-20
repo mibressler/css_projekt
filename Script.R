ecbfa80c9e72318c321bfde2854b26c8493b5559
Michael Bressler <53796824+mbressler@users.noreply.github.com>
2020-12-20 22:23:25 +0100

    Update Script.R

--- Script.R
+++ Script.R
@@ -3,10 +3,13 @@
 library(tidyverse)
 library(readr)
 coronaNet <- read.csv('data/coronanet_release.csv')
-casedata <- read.csv('data/owid-covid-data.csv')
+casedata <- read.csv('data/rki_basic.csv')
 
-germanycase <- casedata %>% filter (location == "Germany")
 germany <- coronaNet %>% filter (country == "Germany")
 x <- table(germany$type)
 
 plot(x)
+
+as.Date(casedata$Meldedatum)
+
+ggplot(data=)
