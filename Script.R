997bc3d80a992f68b3dcc292fbcf7abdc1e0d816
JoCSS2020 <75578821+JoCSS2020@users.noreply.github.com>
2020-12-20 22:45:09 +0100

    Update Script.R

--- Script.R
+++ Script.R
@@ -3,13 +3,17 @@
 library(tidyverse)
 library(readr)
 coronaNet <- read.csv('data/coronanet_release.csv')
-casedata <- read.csv('data/rki_basic.csv')
+casedata <- read.csv('data/owid-covid-data.csv')
 
+germanycase <- casedata %>% filter (location == "Germany")
 germany <- coronaNet %>% filter (country == "Germany")
 x <- table(germany$type)
 
 plot(x)
 
-as.Date(casedata$Meldedatum)
 
-ggplot(data=casedata[,c(7,9), aes(x="Meldedatum", y="AnzahlFall")]) + geom_point()
+d1 <- as.Date("2020-11-11")
+d2 <- as.Date("2020-11-22")
+germanyd <- subset(germanycase, date>d1 & date<d2)
+ggplot(data=germanyd, aes(date, total_cases))+
+  geom_bar(fill="steelblue", stat='identity')
