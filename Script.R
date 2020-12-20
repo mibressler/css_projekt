e6093d69658196cb056036e877038b3afbe2b79c
JoCSS2020 <75578821+JoCSS2020@users.noreply.github.com>
2020-12-20 22:49:36 +0100

    Update Script.R

--- Script.R
+++ Script.R
@@ -13,8 +13,12 @@ plot(x)
 as.Date(casedata$Meldedatum)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 qplot(Meldedatum,y=AnzahlFall, data=casedata)
+=======
+ggplot(data=casedata[,c(7,9), aes(x="Meldedatum", y="AnzahlFall")]) + geom_point()
+>>>>>>> parent of 997bc3d... Update Script.R
 =======
 ggplot(data=casedata[,c(7,9), aes(x="Meldedatum", y="AnzahlFall")]) + geom_point()
 >>>>>>> parent of 997bc3d... Update Script.R
